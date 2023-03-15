ecb236432e1b53d78525ea14bbd89430daa17066
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-15 17:43:46 +0200

    Done AboveTheSea flex layou

--- src/components/Layout/pages/Home_page/Above_the_sea.js
+++ src/components/Layout/pages/Home_page/Above_the_sea.js
@@ -3,22 +3,24 @@ import ReadMore from "../../../Read_more";
 
 function AboveTheSea () {
     return (
-        <section>
+        <section className="above-the-sea">
             <Container classNames={[ "above-the-sea-image-container" ]}>
                 <img src="https://media.cntraveler.com/photos/5798c04651b1fe8b3ce4dbfe/master/pass/mag-tommy-clarke-water-1.jpg" alt="Sea image" />
             </Container>
 
             <Container classNames={[ "above-the-sea-text" ]}>
-                <h2 className="title">Above The Sea</h2>
-                <blockquote>
-                    Ad lorem aliquid laoreet pri, possim iisque ad vim. Te graecis maluisset eum
+                <h1 className="title">Above The Sea</h1>
+                <blockquote className="quote">
+                    <p className="text quote__text">
+                        Ad lorem aliquid laoreet pri, possim iisque ad vim. Te graecis maluisset eum
+                    </p>
                 </blockquote>
 
-                <p>
+                <p className="text text-small text-bold">
                     Sit no platonem consetetur mediocritatem, in alienum.Lorem ipsum dolor sit amet, te eam oratio imperdiet, nam an vidisse admodum omittantur, nihil corrumpitan cum. Cu iriure dignissim consequat quo, usu id ceteros ponderum. Ea vel malis iudico quodsi, ex nam error suscipit prodesset. Qui an deserunt imperdiet. Case temporibus qui ut.
                 </p>
 
-                <p>
+                <p className="text text-small text-bold">
                     Definitiones ad vim. Et case quodsi complectitur ius, in vel regione viderer. Cu sed fugit adolescens. Ad mea latine percipit, unum dicat habemus et mel. Cu eum causae bonorum. Te pri ornatus probatus, an postea nemore iuvaret sea. Mea te rebum virtute, ullum facilisi comprehensam nec eu. At facer latine nec, te nonumy disputationi reprehendunt usu, ut nam unum integre qualisque. Nulla possit mandamus ne vix.
                 </p>
 
