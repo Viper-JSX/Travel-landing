5e64857ca47f8f0a808b3f6e3616ee4d8e969917
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-16 20:59:26 +0200

    Card text content wrapped by a container, added styles for card image

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
 import { TEXT } from "../../various_things/constants";
+import Container from "../Container";
 import ReadMore from "../Read_more";
 
 function Card ({ card={}, classNames=[] }) {
     return (
         <div className={`card ${classNames.join(" ")}`}>
             <img src={ card.imgUrl } alt="Card image" />
-            <h2 className="card__title">{ card.title }</h2>
-            {
-                card.country ? 
-                <b className="contry-text">{card.country}</b>
-                :
-                null                
-            }
-            <p className="card__text text text-small text-bold" >{ card.text }</p>
 
-            {
-                card.readMore ? 
-                <ReadMore to={card.readMore} type={ TEXT } />
-                :
-                null
-            }
+            <Container classNames={[ "card__text" ]}>
+                <h2 className="card__title">{ card.title }</h2>
+                {
+                    card.country ? 
+                    <b className="contry-text">{card.country}</b>
+                    :
+                    null                
+                }
+                <p className="card__text text text-small text-bold" >{ card.text }</p>
+
+                {
+                    card.readMore ? 
+                    <ReadMore to={card.readMore} type={ TEXT } />
+                    :
+                    null
+                }
+            </Container>
         </div>
     );
 }
