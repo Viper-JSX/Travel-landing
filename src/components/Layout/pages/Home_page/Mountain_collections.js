ac9f950d41bd4facc28c2f4c5f9082a28223d98a
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-17 08:57:01 +0200

    Added mountain collections layout styling

--- src/components/Layout/pages/Home_page/Mountain_collections.js
+++ src/components/Layout/pages/Home_page/Mountain_collections.js
@@ -5,17 +5,19 @@ import Container from "../../../Container";
 function MountainCollections () {
     return (
         <section className="mountain-collections">
-            <Container classNames={[ "mountain-collections__text" ]}>
-                <h1>Mountain collections</h1>
-                <p>
-                    See our best mountain collections, explore and Lorem impus dolor sit amet, te eam oratio imperdiet, nam an vidisse admodum omittantur, nihil corrumpit an cum.
-                </p>
-            </Container>
+            <Container classNames={[ "mountain-collections__inner" ]}>   
+                <Container classNames={[ "mountain-collections__text" ]}>
+                    <h1>Mountain collections</h1>
+                    <p>
+                        See our best mountain collections, explore and Lorem impus dolor sit amet, te eam oratio imperdiet, nam an vidisse admodum omittantur, nihil corrumpit an cum.
+                    </p>
+                </Container>
 
-            <Container classNames={[ "mountain-collections__cards" ]}>
-                {
-                    mountainCollectionsCards.map((card) => <Card card={ card } classNames={[ "landscape" ]} />)
-                }
+                <Container classNames={[ "mountain-collections__cards" ]}>
+                    {
+                        mountainCollectionsCards.map((card) => <Card card={ card } classNames={[ "landscape" ]} />)
+                    }
+                </Container>
             </Container>
         </section>
     );
