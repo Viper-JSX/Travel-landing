bd68e1308b29f948325b00c0349bf836351082a9
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-19 09:56:47 +0200

    Changed image links

--- src/various_things/slides.js
+++ src/various_things/slides.js
@@ -28,7 +28,7 @@ const editorsPickSlides = [
                 title: "Flatiron",
                 text: "One of the deepest and largest alpine lakes in Germany and one the best outdors place that you never imagine before.",
                 country: "USA",
-                imgUrl: "https://live.staticflickr.com/7330/10344219536_63fc4d6cc1_b.jpg",
+                imgUrl: "https://theflatironbuilding.com/wp-content/uploads/2021/02/newtop.jpg?v=888",
                 readMore: "https://en.wikipedia.org/wiki/Walchsee_(lake)"
             },
     
@@ -41,18 +41,18 @@ const editorsPickSlides = [
             },
     
             {
-                title: "Golder gates",
+                title: "Golden gates",
                 text: "One of the deepest and largest alpine lakes in Germany and one the best outdors place that you never imagine before.",
                 country: "China",
-                imgUrl: "https://live.staticflickr.com/7330/10344219536_63fc4d6cc1_b.jpg",
+                imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
                 readMore: "https://en.wikipedia.org/wiki/Walchsee_(lake)"
             },
 
             {
-                title: "Canion",
+                title: "Antelope Canion",
                 text: "One of the deepest and largest alpine lakes in Germany and one the best outdors place that you never imagine before.",
                 country: "China",
-                imgUrl: "https://live.staticflickr.com/7330/10344219536_63fc4d6cc1_b.jpg",
+                imgUrl: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/7d/59/9d/photo4jpg.jpg?w=1200&h=1200&s=1",
                 readMore: "https://en.wikipedia.org/wiki/Walchsee_(lake)"
             },
     
@@ -60,7 +60,7 @@ const editorsPickSlides = [
                 title: "Arctica",
                 text: "One of the deepest and largest alpine lakes in Germany and one the best outdors place that you never imagine before.",
                 country: "China",
-                imgUrl: "https://live.staticflickr.com/7330/10344219536_63fc4d6cc1_b.jpg",
+                imgUrl: "https://img.traveltriangle.com/blog/wp-content/uploads/2020/01/Places-To-Visit-In-Switzerland-In-Winter_20th-jun.jpg",
                 readMore: "https://en.wikipedia.org/wiki/Walchsee_(lake)"
             },
         ]
