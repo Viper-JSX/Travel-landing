26a95feed46cb1702c02e82bb7a1d946b8923f98
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-19 11:04:51 +0200

    Created title for sort criterias in editors pick

--- src/components/SortBy.js
+++ src/components/SortBy.js
@@ -1,9 +1,41 @@
+import { DO_NOT_SORT, SORT_BY_NAME, SORT_BY_TITLE } from "../various_things/constants";
+
 function SortBy ({ currentCriteria, sortCriterias, handleSortCriteriaChange }) {
     return (    
         <select value={ currentCriteria } onChange={ (event) => handleSortCriteriaChange({ criteria: event.target.value }) }>
             {
-                sortCriterias.map((criteria) =>
-                    <option value={ criteria } key={criteria}>{criteria}</option>
+                sortCriterias.map((criteria) => {
+                    let criteriaName;
+
+                    switch (criteria) {
+                        case DO_NOT_SORT: {
+                            criteriaName = "Sort By";
+                            break;
+                        }
+
+                        case SORT_BY_NAME: {
+                            criteriaName = "Name";
+                            break;
+                        }
+
+                        case SORT_BY_TITLE: {
+                            criteriaName = "Title";
+                            break;
+                        }
+
+                        case DO_NOT_SORT: {
+                            criteriaName = "Country";
+                            break;
+                        }
+
+                        default: {
+                            criteriaName = "Sort By";
+                        }
+                    }
+
+                    return <option value={ criteria } key={criteria}>{ criteriaName }</option>;
+                }
+        
                 )
             }
         </select>
