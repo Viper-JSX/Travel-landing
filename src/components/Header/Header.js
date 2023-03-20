af492d3c892b35ebc4e2f6c109250532eb91402a
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-20 17:57:53 +0200

    Intro image made responsive. Created media query for 980px

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,3 @@
-import Navigation from "../Navigation/Navigation";
-import NavigationItem from "../Navigation/Navigation_item";
-import UserInfo from "./User_info";
-
 function Header () {
     return (
         <header className="header">
