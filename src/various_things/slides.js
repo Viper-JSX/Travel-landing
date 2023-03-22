893c1ae613dd5a78984a317130a7a789cc362d05
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-03-22 10:41:35 +0200

    Changed image

--- src/various_things/slides.js
+++ src/various_things/slides.js
@@ -7,16 +7,16 @@ const introSlides = [
     },
 
     {
-        title: "The Walcher Lake",
+        title: "Commando Mountain",
         text: "One of the deepest and largest alpine lakes in Germany and one the best outdors place that you never imagine before.",
         imgUrl: "https://d36tnp772eyphs.cloudfront.net/blogs/1/2015/04/Mount-Taranaki-volcano-in-New-Zealand.jpg",
         readMore: "https://en.wikipedia.org/wiki/Walchsee_(lake)"
     },
 
     {
-        title: "The Walcher Lake",
+        title: "Snowy Mountains",
         text: "One of the deepest and largest alpine lakes in Germany and one the best outdors place that you never imagine before.",
-        imgUrl: "https://live.staticflickr.com/7330/10344219536_63fc4d6cc1_b.jpg",
+        imgUrl: "https://rare-gallery.com/uploads/posts/5405063-mountain-peak-night-dusk-star-sky-wallpaper-desktop-wallpapers-nature-wallpapers-desktop-background-desktop-backgrounds-wallpapers-amazing-wallpaper-nature-backgrounds-snow-sunrise-landscape-m.jpg",
         readMore: "https://en.wikipedia.org/wiki/Walchsee_(lake)"
     }
 ];
